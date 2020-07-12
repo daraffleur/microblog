40c360467d2b7f58b4025d1e11deb87680a1e7af
daraffleur <romaniuk.dana@gmail.com>
2020-07-12 20:28:01 +0300

    Receiving login credentials

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,7 +1,7 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, flash
 
-from microblog.app.forms import LoginForm
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -24,9 +24,12 @@ def index():
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit:
+        flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
 
 
