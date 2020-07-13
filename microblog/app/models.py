6b10e4020679189203842d458979ad895203a26f
daraffleur <romaniuk.dana@gmail.com>
2020-07-13 17:52:36 +0300

    Add Flask-Login user mixin class

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -1,11 +1,12 @@
 from datetime import datetime
 
+from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from app import db
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
