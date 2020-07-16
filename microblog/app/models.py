df2d4062472736e07a5cf99699b69e0b025fcc5e
daraffleur <romaniuk.dana@gmail.com>
2020-07-16 22:11:50 +0300

    Fix followed collumn

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -21,10 +21,11 @@ class User(UserMixin, db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
-    followed = db.relationship('User', secondary=followers,
-                               primaryjoin=(followers.c.follower_id == id),
-                               secondaryjoin=(followers.c.follower_id == id),
-                               backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
     def __repr__(self):
         return f'User {self.username}'
