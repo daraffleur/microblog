c06524d342f31ce1fffa3595b9696d3376e1d94b
daraffleur <romaniuk.dana@gmail.com>
2020-07-17 12:39:15 +0300

    Followed posts query with user's own posts

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -44,12 +44,6 @@ class User(UserMixin, db.Model):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'.format(digest, size)
 
-    def followed_posts(self):
-        return Post.query.join(
-            followers, (followers.c.followed_id == Post.user_id)).filter(
-            followers.c.follower_id == self.id).order_by(
-            Post.timestamp.desc())
-
     def follow(self, user):
         if not self.is_folowing(user):
             self.followed.append(user)
@@ -61,6 +55,13 @@ class User(UserMixin, db.Model):
     def is_following(self, user):
         return self.followed.filter(
             followers.c.followed_id == user.id).count() > 0
+
+    def followed_posts(self):
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+            followers.c.follower_id == self.id)
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Post.timestamp.desc())
 
 
 class Post(db.Model):
