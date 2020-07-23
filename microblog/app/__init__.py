a9ee8abe94a78047b61f116bbd26adc5d08d0d8c
daraffleur <romaniuk.dana@gmail.com>
2020-07-23 12:52:40 +0300

    Flask-Babel instance

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -3,6 +3,7 @@ import os
 from logging.handlers import SMTPHandler, RotatingFileHandler
 
 from flask import Flask
+from flask_babel import Babel
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_mail import Mail
@@ -21,6 +22,7 @@ login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+babel = Babel(app)
 
 if not app.debug:
 
