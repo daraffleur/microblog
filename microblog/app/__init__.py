536420371b2b41c96c576a49fa1dcd3c50ffcfb6
daraffleur <romaniuk.dana@gmail.com>
2020-07-23 16:29:32 +0300

    Change language

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -29,9 +29,8 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale():
-    # return request.accept_languages.best_match(app.config['LANGUAGES'])
-    return 'ukr'
-
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    # return 'ukr'
 
 if not app.debug:
 
