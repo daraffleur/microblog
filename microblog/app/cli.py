dc81c679fd983c14144a28405f72e4e9876de382
daraffleur <romaniuk.dana@gmail.com>
2020-07-23 16:42:51 +0300

    Update and compile sub-commands in cli.py module

--- microblog/app/cli.py
+++ microblog/app/cli.py
@@ -1,3 +1,5 @@
+import os
+
 from app import app
 
 
@@ -5,3 +7,20 @@ from app import app
 def translate():
     """Translation and localization commands."""
     pass
+
+
+@translate.command()
+def update():
+    """Update all languages."""
+    if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
+        raise RuntimeError('extract command failed')
+    if os.system('pybabel update -i messages.pot -d app/translations'):
+        raise RuntimeError('update command failed')
+    os.remove('messages.pot')
+
+
+@translate.command()
+def compile():
+    """Compile all languages."""
+    if os.system('pybabel compile -d app/translations'):
+        raise RuntimeError('compile command failed')
