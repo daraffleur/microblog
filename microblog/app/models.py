acf7696ca5078131e10036c6979d959bf86a4817
daraffleur <romaniuk.dana@gmail.com>
2020-07-31 12:57:41 +0300

    Notification model

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -1,3 +1,4 @@
+import json
 from datetime import datetime
 from hashlib import md5
 from time import time
@@ -80,6 +81,8 @@ class User(UserMixin, db.Model):
                                         foreign_keys='Message.recipient_id',
                                         backref='recipient', lazy='dynamic')
     last_message_read_time = db.Column(db.DateTime)
+    notifications = db.relationship('Notification', backref='user',
+                                    lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -161,3 +164,14 @@ class Message(db.Model):
 
     def __repr__(self):
         return '<Message {}>'.format(self.body)
+
+
+class Notification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    timestamp = db.Column(db.Float, index=True, default=time)
+    payload_json = db.Column(db.Text)
+
+    def get_data(self):
+        return json.loads(str(self.payload_json))
