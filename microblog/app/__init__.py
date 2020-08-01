7d048be84fc6dfbbcbf6bb7543c6ffa741be552b
daraffleur <romaniuk.dana@gmail.com>
2020-08-01 16:49:49 +0300

    RQ integration

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,16 +1,19 @@
 import logging
-from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
+from logging.handlers import SMTPHandler, RotatingFileHandler
 
+import rq
 from elasticsearch import Elasticsearch
 from flask import Flask, request, current_app
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from flask_babel import Babel, lazy_gettext as _l
+from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_mail import Mail
-from flask_bootstrap import Bootstrap
+from flask_migrate import Migrate
 from flask_moment import Moment
-from flask_babel import Babel, lazy_gettext as _l
+from flask_sqlalchemy import SQLAlchemy
+from redis import Redis
+
 from config import Config
 
 db = SQLAlchemy()
@@ -37,6 +40,8 @@ def create_app(config_class=Config):
     babel.init_app(app)
     app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
         if app.config['ELASTICSEARCH_URL'] else None
+    app.redis = Redis.from_url(app.config['REDIS_URL'])
+    app.task_queue = rq.Queue('microblog-tasks', connection=app.redis)
 
     from app.errors import bp as errors_bp
     app.register_blueprint(errors_bp)
